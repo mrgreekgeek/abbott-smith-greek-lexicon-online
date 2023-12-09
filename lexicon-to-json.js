619bb3353e7aebc9a59c2d3d3cf6cdc7f258331b
Jeffrey N <fuishpo@protonmail.com>
2023-12-08 23:07:42 -0500

    Indexing info for the lexicon entries is now displayed.

--- lexicon-to-json.js
+++ lexicon-to-json.js
@@ -20,12 +20,63 @@ function encodeLexicon(lexiconURL) {
         let parser = new DOMParser();
         let xmlDoc = parser.parseFromString(data, "text/xml");
         let lexiconEntries = xmlDoc.getElementsByTagName('entry');
+        
+        //many <entry>ies in the lexicon are not for words
+        lexiconEntries = Array.from(lexiconEntries)
+          .filter((entry) => {
+            return entry.hasAttribute("lemma");
+          });
 
-        return Promise.resolve(indexLexicon(lexiconEntries));
+        return indexLexicon(lexiconEntries);
         });
   }
 
 
 function indexLexicon(lexiconEntries) {
-  return "indexLexicon does nothing yet...";
+  let curEntry = null,
+    entries = new Array(),
+    searchTerms = new Object(),
+    termsToIndex = null;
+  
+  //create callbacks
+  let callbacks = [
+    //index by greek spelling
+    (entry) => {
+      let greekWord = entry.getAttribute('lemma').split('|')[0];
+      
+      greekWord = normalizeGreek(greekWord);
+      
+      return [greekWord];//, latinWord];
+      },
+    //index by strongs number
+    (entry) => {
+      let lemmaNumbers = null,
+        numbers = [entry.getAttribute('strong')]; //main strong number
+      
+      lemmaNumbers = entry.getAttribute('lemma')
+        .split('|')
+        .splice(1);
+      
+      numbers = numbers.concat(lemmaNumbers);
+      
+      return numbers;
+      },
+    ];
+  
+  // save the valid search terms from each entry
+  for (var i = 0; i < lexiconEntries.length; i++) {
+    curEntry = lexiconEntries[i];
+    entries.push(curEntry);
+    
+    for (var func in callbacks) {
+      termsToIndex = callbacks[func](curEntry);
+      
+      for (var term in termsToIndex) {
+        let cTerm = termsToIndex[term];
+        searchTerms[cTerm] = i;
+        }
+      }
+    }
+  
+  return searchTerms;
   }
